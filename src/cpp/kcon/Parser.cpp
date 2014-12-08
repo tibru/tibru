6113d03218b003327ae367420d24424c2c8cdfec
Peter <peter@git.com>
2014-12-08 11:21:14 +0000

    Shell of reverse method in parser

--- src/cpp/kcon/Parser.cpp
+++ src/cpp/kcon/Parser.cpp
@@ -42,9 +42,39 @@ pcell_t Parser::_parse_elems( std::istream& is )
 	return pcell_t::null();
 }
 
-pcell_t Parser::_reverse_and_reduce( pcell_t p )
+pcell_t Parser::_reverse_and_reduce( pcell_t pcell )
 {
-    return p;
+	switch( pcell.typecode() )
+	{
+		case Cell<pcell_t,pcell_t>::TYPECODE:
+		{
+            const Cell<pcell_t,pcell_t>* pcc = pcell.cast<pcell_t,pcell_t>();
+			return new (_alloc) Cell<pcell_t,pcell_t>{ pcc->head, pcc->tail };
+			break;
+		}
+		case Cell<pcell_t,value_t>::TYPECODE:
+		{
+            const Cell<pcell_t,value_t>* pcv = pcell.cast<pcell_t,value_t>();
+			return new (_alloc) Cell<pcell_t,value_t>{ pcv->head, pcv->tail };
+			break;
+		}
+		case Cell<value_t,pcell_t>::TYPECODE:
+		{
+            const Cell<value_t,pcell_t>* pvc = pcell.cast<value_t,pcell_t>();
+			return new (_alloc) Cell<value_t,pcell_t>{ pvc->head, pvc->tail };
+			break;
+		}
+		case Cell<value_t,value_t>::TYPECODE:
+		{
+            const Cell<value_t,value_t>* pvv = pcell.cast<value_t,value_t>();
+			return new (_alloc) Cell<value_t,value_t>{ pvv->head, pvv->tail };
+			break;
+		}
+		default:
+			error( "dispatch failed" );
+	}
+
+	return pcell_t::null();
 }
 
 pcell_t Parser::parse( std::istream& is )
