afefa44aa18df2ef01fbc6366844ac920d80d9c8
Peter <peter@git.com>
2014-12-15 21:46:46 +0000

    Removing use of typecode in Allocator

--- src/cpp/kcon/Allocator.cpp
+++ src/cpp/kcon/Allocator.cpp
@@ -9,31 +9,11 @@ void SimpleAllocator::_mark( std::set<pcell_t>& live, pcell_t p )
 
     live.insert( p.addr() );
 
-    switch( p->typecode() )
-    {
-		case CellType<pcell_t,pcell_t>::TYPECODE:
-		{
-			_mark( live, p->head().pcell() );
-			_mark( live, p->tail().pcell() );
-			break;
-		}
-		case CellType<pcell_t,value_t>::TYPECODE:
-		{
-		    _mark( live, p->head().pcell() );
-		    break;
-		}
-		case CellType<value_t,pcell_t>::TYPECODE:
-		{
-		    _mark( live, p->tail().pcell() );
-		    break;
-		}
-		case CellType<value_t,value_t>::TYPECODE:
-		{
-		    break;
-		}
-		default:
-			throw Error<Runtime>( "mark dispatch failed" );
-    }
+    if( p->head().is_pcell() )
+        _mark( live, p->head().pcell() );
+
+    if( p->tail().is_pcell() )
+        _mark( live, p->tail().pcell() );
 }
 
 void SimpleAllocator::gc( const Roots& roots )
