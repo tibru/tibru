db7f1202897d268ffec851121a8c74b91f4338ff
Peter <peter@git.com>
2014-12-15 21:16:19 +0000

    Simpler kstack

--- src/cpp/kcon/kstack.h
+++ src/cpp/kcon/kstack.h
@@ -3,25 +3,20 @@
 
 namespace kcon {
 
-template<class T>
-class kstack
+class basic_kstack
 {
+protected:
     Allocator& _alloc;
     pcell_t _items;
-public:
-    kstack( Allocator& alloc )
-        : _alloc( alloc ), _items( pcell_t::null() ) {}
 
+    basic_kstack( Allocator& alloc )
+        : _alloc( alloc ), _items( pcell_t::null() ) {}
+public:
     pcell_t& items() { return _items; }
 
-    void push( const T& item, Allocator::Roots roots )
+    void push( elem_t item, Allocator::Roots roots )
     {
         _items = new (_alloc,roots) Cell{ item, _items };
-    }
-
-    const T& top()
-    {
-        return _items->head.pcell;
     }
 
     void pop()
@@ -32,54 +27,31 @@ public:
     bool empty() const { return _items == pcell_t::null(); }
 };
 
+template<class T>
+struct kstack;
+
 template<>
-class kstack<elem_t>
+struct kstack<elem_t> : basic_kstack
 {
-    Allocator& _alloc;
-    pcell_t _pcell_items;
-    pcell_t _value_items;
-    pcell_t _which_items;
-public:
     kstack( Allocator& alloc )
-        : _alloc( alloc ), _pcell_items( pcell_t::null() ), _value_items( pcell_t::null() ), _which_items( pcell_t::null() ) {}
+        : basic_kstack( alloc ) {}
 
-    pcell_t& items() { return _pcell_items; }
-
-    void push( const elem_t& item, Allocator::Roots roots )
+    const elem_t& top()
     {
-        if( item.is_pcell() )
-        {
-            _pcell_items = new (_alloc,roots) Cell{ item.pcell, _pcell_items };
-            _which_items = new (_alloc,roots) Cell{ byte_t(0), _which_items };
-        }
-        else
-        {
-            _value_items = new (_alloc,roots) Cell{ item.value, _value_items };
-            _which_items = new (_alloc,roots) Cell{ 1, _which_items };
-        }
+        return _items->head;
     }
+};
 
-    elem_t top()
+template<>
+struct kstack<pcell_t> : basic_kstack
+{
+    kstack( Allocator& alloc )
+        : basic_kstack( alloc ) {}
+
+    const pcell_t& top()
     {
-        value_t w = _which_items->head.value;
-        if( w == 0 )
-            return _pcell_items->head;
-        else
-            return _value_items->head;
+        return _items->head.pcell;
     }
-
-    void pop()
-    {
-        value_t w = _which_items->head.value;
-        if( w == 0 )
-            _pcell_items = _pcell_items->tail.pcell;
-        else
-            _value_items = _value_items->tail.pcell;
-
-        _which_items = _which_items->tail.pcell;
-    }
-
-    bool empty() const { return _pcell_items == pcell_t::null(); }
 };
 
 }   //namespace
