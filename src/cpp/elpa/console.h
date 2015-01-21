f4a5c09371e37a41c2540a08255d6d92f221ba37
Peter <peter@git.com>
2015-01-21 14:24:57 +0000

    Abstracting console

---
+++ src/cpp/elpa/console.h
@@ -0,0 +1,20 @@
+#ifndef CONSOLE_H_INCLUDED
+#define CONSOLE_H_INCLUDED
+
+#include "common.h"
+
+namespace elpa {
+
+struct Console
+{
+    static auto help( int ret_code ) -> int;
+
+    template<class Env>
+    static auto run( size_t ncells, const std::vector< std::string >& filenames, bool noisy, bool repl ) -> int;
+
+    static auto go( int argc, const char* argv[] ) -> int;
+};
+
+}   //namespace
+
+#endif // CONSOLE_H_INCLUDED
